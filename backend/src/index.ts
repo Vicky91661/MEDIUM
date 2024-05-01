439432a3d201b5e70dbec35bf897ece28d62d42b
Vicky91661 <kumar.vickysah72@gmail.com>
2024-05-01 06:57:12 +0530

    Added gitigore and readme file and cloudworker backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,47 @@
-import express from "express";
+import { Hono } from 'hono'
 
-const app = express();
 
-app.listen(3000,()=>{
-    console.log("listining to port 3000")
+// Create the main Hono app
+const app = new Hono();
+
+// POST /api/v1/user/signup
+app.post('/api/v1/signup', (c) => {
+	return c.text('signup route')
 })
+
+
+// POST /api/v1/user/signin
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
+
+
+// GET /api/v1/blog/:id
+app.get('/api/v1/blog/:id', (c) => {
+	const id = c.req.param('id')
+	console.log(id);
+	return c.text('get blog route')
+})
+
+
+// POST /api/v1/blog
+app.post('/api/v1/blog', (c) => {
+
+	return c.text('signin route')
+})
+
+
+// PUT /api/v1/blog
+app.put('/api/v1/blog', (c) => {
+	return c.text('signin route')
+})
+
+// GET /api/v1/blog/bulk
+app.get('/api/v1/blog/bulk',(c)=>{
+  return c.text('bulk route')
+})
+
+
+
+export default app;
+
